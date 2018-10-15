a469f4d0e28a40676f32380902307393905ba2d5
Joe Karam <joekaram97@gmail.com>
2018-10-15 14:39:22 +0300

    Fixed form bug.

--- resources/js/components/FormComponent.vue
+++ resources/js/components/FormComponent.vue
@@ -1,50 +1,63 @@
- <template>
- 
- <form method="POST" action="/products" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
-         @csrf
-            <div class="control">
-                <label for="name" class="label">Product Name:</label>
-                
-                <input type="text" id="name" name="name" class="input" v-model="form.name"> 
-
-                <span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
-            </div>
-
-            <div class="control">
-                <label for="description" class="label">Product Description:</label>
-                
-                <input type="text" id="description" name="description" class="input" v-model="form.description">
-
-                <span class="help is-danger" v-if="form.errors.has('description')" v-text="form.errors.get('description')"></span>
-            </div>
-
-            <div class="control">
-                <label for="price" class="label">Product Price:</label>
-                
-                <input type="text" id="price" name="price" class="input" v-model="form.price">
-
-                <span class="help is-danger" v-if="form.errors.has('price')" v-text="form.errors.get('price')"></span>
-            </div>
-
-            <div class="control">
-                <label for="URL" class="label">Product URL:</label>
-                
-                <input type="text" id="URL" name="URL" class="input" v-model="form.URL">
-
-                <span class="help is-danger" v-if="form.errors.has('URL')" v-text="form.errors.get('URL')"></span>
-            </div>
-
-            <div class="control">
-                <button class="button is-primary" :disabled="form.errors.any()">Create</button>
-            </div>
-        </form>
-             
+ <template> 
+    <form method="POST" action="/products" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
+        <div class="control">
+            <label for="name" class="label">Product Name:</label>
+            <input type="text" id="name" name="name" class="input" v-model="form.name"> 
+            <span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
+        </div>
+        <div class="control">
+            <label for="description" class="label">Product Description:</label>
+            <input type="text" id="description" name="description" class="input" v-model="form.description">
+            <span class="help is-danger" v-if="form.errors.has('description')" v-text="form.errors.get('description')"></span>
+        </div>
+        <div class="control">
+            <label for="price" class="label">Product Price:</label>
+            <input type="text" id="price" name="price" class="input" v-model="form.price">
+            <span class="help is-danger" v-if="form.errors.has('price')" v-text="form.errors.get('price')"></span>
+        </div>
+        <div class="control">
+            <label for="URL" class="label">Product URL:</label>
+            <input type="text" id="URL" name="URL" class="input" v-model="form.URL">
+            <span class="help is-danger" v-if="form.errors.has('URL')" v-text="form.errors.get('URL')"></span>
+        </div>
+        <div class="control">
+            <button class="button is-primary" :disabled="form.errors.any()">Create</button>
+        </div>
+    </form>
  </template>
 
 <script>
     export default {
+        props: [],
+        data() {
+            return {
+                form: new Form({
+                    name: '',
+                    description: '',
+                    price: ''
+                })
+            }
+        },
         mounted() {
-            console.log('Component mounted.')
+            this.init();
+        },
+        methods: {
+            init() {
+                // 
+            },
+            onSubmit() {
+                this.form.post('/products')
+                    .then(response => alert('Success'))
+                    .catch(error => {
+                        console.log(error.response.data);
+                    });
+            }
+        },
+        computed: {
+            // 
+        },
+        watch: {
+            // 
         }
     }
 </script>
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css">
-    <style>body { padding-top: 40px; }</style>
-</head>
+@extends('layouts.app')
 
-<body>
-    <div id="app" class="container">
-     
-<form-component></form-component>
-
-       
-    </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.15.3/axios.js"></script>
-    <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
-    <script src="/js/app.js"></script>
-</body>
-</html>
-
-
+@section('content')
+    <form-component></form-component>
+@endsection
